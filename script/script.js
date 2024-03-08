330a24d7a87e4e673e7284d8e6a56ba8337ff700
henriquemgoncalves <88dev.hg@gmail.com>
2024-03-08 17:01:55 -0300

    portfolio projeto

---
+++ script/script.js
@@ -0,0 +1,22 @@
+const relogio = document.getElementById("relogio");
+const curriculo = document.getElementById("curriculo");
+const deco = document.getElementById("deco");
+const numero = document.getElementById("numero");
+
+
+
+relogio.addEventListener('click', function(){
+    window.open('https://projeto-relogio-tau.vercel.app/', 'blank');
+});
+
+curriculo.addEventListener('click', function(){
+    window.open('https://curriculum-weld.vercel.app/', 'black');
+});
+
+deco.addEventListener('click', function(){
+    window.open('https://decodificador-texto-henrique.vercel.app/', 'black');
+});
+
+numero.addEventListener('click', function (){
+    window.open('https://jogo-numero-secreto-six-rouge.vercel.app/', 'blank');
+});
